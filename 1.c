6eaef65eb118bd990a2e05d8630670e30aaf7740
Arsentiy Zaginaev <a.zaginaev@g.nsu.ru>
2025-10-14 11:35:47 +0700

    commit

--- 1.c
+++ 1.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/resource.h>
 #include <sys/time.h>
@@ -5,94 +6,90 @@
 #include <ulimit.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-#define GET_LIM 1
-#define SET_LIM 2
+#include <errno.h>
+#include <inttypes.h>
 
 extern char **environ;
 
 int main(int argc, char *argv[]) {
     int c;
     struct rlimit r;
-    char **p;
-    
-    if(argc < 2) {
-        fprintf(stderr,"Use: %s options\n", argv[0]);
+
+    if (argc < 2) {
+        fprintf(stderr, "Use: %s options\n", argv[0]);
         return 1;
     }
-    
-    while((c = getopt(argc, argv, "ispuU:cC:dvV:")) != -1) {
-        switch(c) {
+
+    while ((c = getopt(argc, argv, "ispuU:cC:dvV:")) != -1) {
+        switch (c) {
         case 'i':
-            printf("uid=%ld euid=%ld gid=%ld egid=%ld\n", 
-                   getuid(), geteuid(), getgid(), getegid());
+            printf("uid=%ld euid=%ld gid=%ld egid=%ld\n",
+                   (long)getuid(), (long)geteuid(),
+                   (long)getgid(), (long)getegid());
             break;
+
         case 's':
-            setpgid(0,0);
+            if (setpgid(0, 0) == -1) perror("setpgid");
             break;
+
         case 'p':
-            printf("pid=%ld ppid=%ld pgid=%ld\n", 
-                   getpid(), getppid(), getpgid(0));
+            printf("pid=%ld ppid=%ld pgid=%ld\n",
+                   (long)getpid(), (long)getppid(), (long)getpgid(0));
             break;
+
         case 'U': {
-            if (optarg == NULL) {
-                fprintf(stderr, "Option -U requires argument\n");
-                break;
-            }
-            struct rlimit r;
-            if (getrlimit(RLIMIT_NPROC, &r) == -1) {
-                perror("getrlimit(RLIMIT_NPROC)");
-                break;
-            }
-            r.rlim_cur = (rlim_t)atoll(optarg);  // можно ниже/равно rlim_max
-            if (setrlimit(RLIMIT_NPROC, &r) == -1) {
-                perror("setrlimit(RLIMIT_NPROC)");
-            }
-            break;
-            }
+#ifndef RLIMIT_NPROC
+            fprintf(stderr, "RLIMIT_NPROC is not supported on this system.\n");
+#else
+            if (!optarg) { fprintf(stderr, "-U needs an argument\n"); break; }
+            if (getrlimit(RLIMIT_NPROC, &r) == -1) { perror("getrlimit"); break; }
+            r.rlim_cur = (rlim_t)atoll(optarg);
+            if (setrlimit(RLIMIT_NPROC, &r) == -1) perror("setrlimit");
+#endif
+            break; }
 
         case 'u': {
-            struct rlimit r;
-            if (getrlimit(RLIMIT_NPROC, &r) == -1) {
-                perror("getrlimit(RLIMIT_NPROC)");
-            } else {
-            if (r.rlim_cur == RLIM_INFINITY)
-                printf("ulimit=-1 (unlimited)\n");
-            else
-                printf("ulimit=%llu\n", (unsigned long long)r.rlim_cur);
-            }
-            break;
-            }
+#ifndef RLIMIT_NPROC
+            fprintf(stderr, "RLIMIT_NPROC is not supported on this system.\n");
+#else
+            if (getrlimit(RLIMIT_NPROC, &r) == -1) perror("getrlimit");
+            else if (r.rlim_cur == RLIM_INFINITY) puts("ulimit=-1 (unlimited)");
+            else printf("ulimit=%ju\n", (uintmax_t)r.rlim_cur);
+#endif
+            break; }
+
         case 'c':
-            getrlimit(RLIMIT_CORE, &r);
-            printf("core=%ld\n", r.rlim_cur);
+            if (getrlimit(RLIMIT_CORE, &r) == -1) perror("getrlimit");
+            else if (r.rlim_cur == RLIM_INFINITY) puts("core=unlimited");
+            else printf("core=%ju\n", (uintmax_t)r.rlim_cur);
             break;
+
         case 'C':
-            if(optarg == NULL) {
-                fprintf(stderr, "Option -C requires argument\n");
-                break;
-            }
-            getrlimit(RLIMIT_CORE, &r);
-            r.rlim_cur = atol(optarg);
-            if(setrlimit(RLIMIT_CORE, &r) == -1)
-                fprintf(stderr,"Need root for core\n");
+            if (!optarg) { fprintf(stderr, "-C needs an argument\n"); break; }
+            if (getrlimit(RLIMIT_CORE, &r) == -1) { perror("getrlimit"); break; }
+            r.rlim_cur = (rlim_t)atoll(optarg);
+            if (setrlimit(RLIMIT_CORE, &r) == -1) perror("setrlimit");
             break;
-        case 'd':
-            printf("cwd=%s\n", getcwd(NULL,0));
-            break;
-        case 'v':
-            for(p=environ; *p; p++) printf("%s\n", *p);
-            break;
+
+        case 'd': {
+            char *cwd = getcwd(NULL, 0);
+            if (!cwd) perror("getcwd");
+            else { printf("cwd=%s\n", cwd); free(cwd); }
+            break; }
+
+        case 'v': {
+            for (char **p = environ; *p; ++p) puts(*p);
+            break; }
+
         case 'V':
-            if(optarg == NULL) {
-                fprintf(stderr, "Option -V requires argument\n");
-                break;
-            }
-            putenv(optarg);
+            if (!optarg) { fprintf(stderr, "-V needs NAME=VALUE\n"); break; }
+            if (!strchr(optarg, '=')) { fprintf(stderr, "-V needs NAME=VALUE\n"); break; }
+            if (putenv(optarg) != 0) perror("putenv");
             break;
-        case '?':
-            fprintf(stderr, "Unknown option: -%c\n", optopt);
-            break;
+
+        default:
+            fprintf(stderr, "Unknown option: -%c\n", optopt ? optopt : c);
+            return 1;
         }
     }
     return 0;
