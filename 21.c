ae30e1692916541b10d0466457709de66c371a37
Arsentiy Zaginaev <a.zaginaev@g.nsu.ru>
2025-11-18 16:35:52 +0700

    commit

--- 21.c
+++ 21.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
+#define   BEL   '\07'
 
-int signal_count;
+int count;
 
-void handle_sigint(int sig) {
-    signal_count++;
-    putchar(7);  // Звуковой сигнал
-    fflush(stdout);  // Немедленный вывод
+main()
+{
+    void sigcatch(int);
+
+    signal(SIGINT, sigcatch);
+    signal(SIGQUIT, sigcatch);
+
+    for (;;)
+        pause();
 }
 
-void handle_sigquit(int sig) {
-    printf("\nSIGQUIT received. SIGINT was triggered %d times.\n", signal_count);
-    exit(0);
-}
-
-int main() {
-    if (signal(SIGINT, handle_sigint) == SIG_ERR) {
-        perror("Failed to handle SIGINT");
+void sigcatch(int sig)
+{
+    if (sig == SIGQUIT) {
+        printf("bell was rung %d times\n", count);
         exit(1);
     }
-
-    if (signal(SIGQUIT, handle_sigquit) == SIG_ERR) {
-        perror("Failed to handle SIGQUIT");
-        exit(1);
-    }
-
-    printf("Program is running. Press Ctrl+C to trigger SIGINT and Ctrl+\\ to trigger SIGQUIT.\n");
-
-    while (1) {
-        pause();  // Ожидаем сигналы
-    }
-
-    return 0;
+    printf("%c\n", BEL);
+    count++;
+    signal(sig, sigcatch);
 }
